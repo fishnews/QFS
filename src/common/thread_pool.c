68fc056cec80db1147234b608300f537427e7c39
hz <gzh19881116@163.com>
2016-02-02 23:02:38 +0800

    add thread pool function

--- src/common/thread_pool.c
+++ src/common/thread_pool.c
@@ -32,3 +32,87 @@ static void* thread_routine(void *arg)
 		}
 		return NULL;
 }
+
+int create_thread_pool(int num)
+{
+		int i;
+		pool = (ThreadPool*)malloc(sizeof(ThreadPool));
+		pool->shutdown = 0;
+		pool->max_thread_num = num;
+		pool->head = NULL;
+		pool->tail = NULL;
+		pthread_mutex_init(&pool->mutex, NULL);
+		pthread_cond_init(&pool->cond, NULL);
+
+		pool->thread_id = (pthread_t *)malloc(sizeof(pthread_t) * num);
+		for(i=0; i<num; i++)
+		{
+				if(!pthread_create(&pool->thread_id[i], NULL, thread_routine, NULL))
+				{
+						printf("create thread error\n");
+						exit(1);
+				}
+		}
+		return 0;
+}
+
+int destroy_thread_pool()
+{
+		int i;
+		ThreadWork *member;
+		if(pool->shutdown)
+		{
+				return 0;
+		}
+		pool->shutdown = 1;
+		pthread_mutex_lock(&pool->mutex);
+		pthread_cond_broadcast(&pool->cond);
+		pthread_mutex_unlock(&pool->mutex);
+
+		for(i=0; i<pool->max_thread_num; i++)
+		{
+				pthread_join(pool->thread_id[i], NULL);
+		}
+		free(pool->thread_id);
+
+		while(pool->head)
+		{
+				member = pool->head;
+				pool->head = pool->head->next;
+				free(member->arg);
+				free(member);
+		}
+
+		pthread_mutex_destroy(&pool->mutex);
+		pthread_cond_destroy(&pool->cond);
+		return 0;
+}
+
+int add_work_to_pool(void*(*routine)(void*), void *arg)
+{
+		ThreadWork *work;
+		if(!routine)
+		{
+				printf("function is null\n");
+				return -1;
+		}
+		work = (ThreadWork*)malloc(sizeof(ThreadWork));
+		work->routine = routine;
+		work->arg = arg;
+		work->next = NULL;
+
+		pthread_mutex_lock(&pool->mutex);
+		if(!pool->head)
+		{
+				pool->head = work;
+				pool->tail = work;
+		}
+		else
+		{
+				pool->tail->next = work;
+				pool->tail = work;
+		}
+		pthread_cond_signal(&pool->cond);
+		pthread_mutex_unlock(&pool->mutex);
+		return 0;
+}
