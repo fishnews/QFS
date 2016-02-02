41dad22970e76810fdc658d778f49b466585a67c
hz <gzh19881116@163.com>
2016-02-02 22:19:34 +0800

    changes log

--- src/common/thread_pool.h
+++ src/common/thread_pool.h
@@ -1,3 +1,27 @@
 #ifndef THREAD_POOL_H__
 #define THREAD_POOL_H__
+
+#include <pthread.h>
+
+typedef struct threadWork
+{
+		void*				(*routine)(void *arg);
+		void				*arg;
+		struct threadWork	*next;
+}ThreadWork;
+
+typedef struct threadPool
+{
+		int					shutdown;
+		int					max_thread_num;
+		pthread_t			*thread_id;
+		ThreadWork			*head;
+		ThreadWork			*tail;
+		pthread_mutex_t		mutex;
+		pthread_cond_t		cond;;
+}ThreadPool;
+
+int	create_thread_pool(int num);
+int destroy_thread_pool();
+int add_work_to_pool(void*(*routine)(void*), void *arg);
 #endif
